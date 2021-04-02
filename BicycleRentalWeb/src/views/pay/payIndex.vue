93929118c4ee1c98c36b8a1d8b91eb13c02d45e0
xhwangcr <1102343495@qq.com>
2021-04-02 18:10:48 +0800

    前端支付参数改变

--- BicycleRentalWeb/src/views/pay/payIndex.vue
+++ BicycleRentalWeb/src/views/pay/payIndex.vue
@@ -5,15 +5,15 @@
         <span>支付</span>
       </div>
       <div class="aaa" style="margin: 10px 50px">
-        <form :model="form" label-width="85px" action="http://localhost:9001/aliPay" method="post">
+        <form :model="form" label-width="85px" action="http://localhost:9001/aliPay" method="post"  target="_blank">
           <span>订单号:</span>
-          <el-input v-model="form.orderId" name="Bout_trade_no"></el-input>
+          <el-input v-model="form.orderId" name="out_trade_no"></el-input>
           <br/>
           <span>自行车名:</span>
-          <el-input v-model="form.bicycle.bicycleName" name="Btotal_amount"></el-input>
+          <el-input v-model="form.bicycle.bicycleName" name="subject"></el-input>
           <br/>
           <span>自行车描述:</span>
-          <el-input v-model="form.bicycle.describ" name="Bbody"></el-input>
+          <el-input v-model="form.bicycle.describ" name="body"></el-input>
           <br/>
           <span>租赁方式:</span>
           <el-input v-model="form.rentMode"></el-input>
@@ -25,7 +25,7 @@
           <el-input v-model="form.endTime"></el-input>
           <br/>
           <span>租金:</span>
-          <el-input v-model="form.rent" name="Bsubject"></el-input>
+          <el-input v-model="form.rent" name="total_amount"></el-input>
           <br/>
           <button type="submit"
                   style="text-align: center;background-color: #409EFF;width: 80px;height: 40px;margin-top: 20px"
